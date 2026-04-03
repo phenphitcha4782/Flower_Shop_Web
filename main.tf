c539b9ff2496cc9961da11bd13c96bc9124bd030
phenphitcha4782 <phenphitchaw66@nu.ac.th>
2026-04-04 00:06:44 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,12 @@ resource "aws_security_group" "web_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+   ingress {
+    from_port   = 5173
+    to_port     = 5173
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
