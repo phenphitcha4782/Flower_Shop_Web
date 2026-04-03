22ff6695c1c27c14d2619f1b4b32b6a1b71d61bd
phenphitcha4782 <phenphitchaw66@nu.ac.th>
2026-04-03 22:44:02 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,14 @@
 output "web_ip" {
-  value = aws_instance.web.public_ip
+  description = "Public IP ของ EC2"
+  value       = aws_instance.web.public_ip
+}
+
+output "web_url" {
+  description = "URL สำหรับเข้าเว็บ"
+  value       = "http://${aws_instance.web.public_ip}:3000"
 }
 
 output "db_endpoint" {
-  value = aws_db_instance.mysql.endpoint
+  description = "MySQL endpoint"
+  value       = aws_db_instance.mysql.endpoint
 }
